2617ac1ad464b2a63ad7d3d20c1f674302532fcc
yuanjin <yuanjinmoke@gmail.com>
2019-04-27 13:01:47 +0800

    在React中使用TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CountComp } from './components/CountComp';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+    num:number
 }
 
-export default App;
+export class App extends React.Component<{}, IState> {
+    state: IState = {
+        num: 0
+    }
+    render() {
+        return (
+            <CountComp num={this.state.num} onChange={n => {
+                this.setState({
+                    num: n
+                })
+            }} />
+        )
+    }
+}
